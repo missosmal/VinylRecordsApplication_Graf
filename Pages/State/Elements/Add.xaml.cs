d26b5fa0be1bf6bd350b8dcf0eed1e1c4fc63aa3
diana12wndb <dibragimova662@gmail.com>
2024-04-08 15:36:44 +0500

    Разработанная страница добавления и изменения состояний. Graf

--- Pages/State/Elements/Add.xaml.cs
+++ Pages/State/Elements/Add.xaml.cs
@@ -20,9 +20,56 @@ namespace VinylRecordsApplication_Graf.Pages.State.Elements
     /// </summary>
     public partial class Add : Page
     {
-        public Add()
+        Classes.State changeState;
+        public Add(Classes.State state = null)
         {
             InitializeComponent();
+            if(state != null)
+            {
+                this.changeState = state;
+                this.tbName.Text = state.Name;
+                this.tbSubname.Text = state.Subname;
+                this.tbDescription.Text = state.Description;
+                addBth.Content = "Изменить";
+            }
+        }
+        private void AddState(object sender, RoutedEventArgs e)
+        {
+            if(!String.IsNullOrEmpty(tbName.Text))
+            {
+                if (!String.IsNullOrEmpty(tbSubname.Text))
+                {
+                    if (this.changeState == null)
+                    {
+                        Classes.State newState = new Classes.State()
+                        {
+                            Name = tbName.Text,
+                            Subname = tbSubname.Text,
+                            Description = tbDescription.Text
+                        };
+
+                        newState.Save();
+                        MessageBox.Show($"Состояние {newState.Name} успешно добавлено.", "Уведомление");
+                        MainWindow.mainWindow.OpenPage(new Add(newState));
+                    }
+                    else
+                    {
+                        changeState.Name = tbName.Text;
+                        changeState.Subname = tbSubname.Text;
+                        changeState.Description = tbDescription.Text;
+                        changeState.Save(true);
+                        MessageBox.Show($"Состояние {changeState.Name} успешно изменено.", "Уведомление");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Пожалуйста, укажите сокращенное наименование состояния.", "Уведомление");
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Пожалуйста, укажите наименование состояния.", "Уведомление");
+            }
         }
     }
 }
